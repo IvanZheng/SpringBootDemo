4e25e20374384dd679e08581ea17f665d66d7a24
Ivan Zheng <yvonne_7_7@hotmail.com>
2019-11-12 14:37:09 +0800

    add log4j

--- src/SpringBootDemo.Portal/src/main/java/com/demo/portal/controllers/UserController.java
+++ src/SpringBootDemo.Portal/src/main/java/com/demo/portal/controllers/UserController.java
@@ -5,27 +5,31 @@ import com.demo.domain.models.User;
 import com.demo.infrastructure.Page;
 import com.demo.portal.models.UserDto;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     final UserService userService;
-
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public UserDto get(@PathVariable String id) {
+        logger.debug("get user {}", id);
         User user = userService.selectUser(id);
         return new UserDto(id, user.getName());
     }
 
     @RequestMapping(value = "/{pageIndex}/{pageSize}")
-    public Page<UserDto> get(@PathVariable Integer pageIndex,
+    public Page<UserDto> get(@PathVariable int pageIndex,
                              @PathVariable int pageSize,
                              @RequestParam(required = false) String name) {
+        logger.debug("get users {} {} {}", pageIndex, pageSize, name);
+
         Page<UserDto> page = new Page<>(pageIndex, pageSize);
         page.getParams().put("name", name);
         UserDto[] users = userService.selectUsers(page)
