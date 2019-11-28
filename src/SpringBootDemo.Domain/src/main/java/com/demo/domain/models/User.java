db4b4aa83e421fcdb575141a559749667bd9e5af
Ivan <yvonne_7_7@hotmail.com>
2019-11-28 21:08:54 +0800

    clean up code

--- src/SpringBootDemo.Domain/src/main/java/com/demo/domain/models/User.java
+++ src/SpringBootDemo.Domain/src/main/java/com/demo/domain/models/User.java
@@ -2,9 +2,7 @@ package com.demo.domain.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,73 +13,58 @@ public class User {
     private String gender;
     private UserProfile userProfile;
     private List<Card> cards;
-    private Timestamp Version;
+    private Timestamp version;
 
     public User() {
     }
 
     public User(String name, String gender, UserProfile userProfile, List<Card> cards) {
-        setId(UUID.randomUUID().toString());
+        this.id = UUID.randomUUID().toString();
         this.name = name;
         this.gender = gender;
         this.userProfile = userProfile;
         setCards(cards);
     }
 
-    public void update(String name, String gender, UserProfile userProfile)
-    {
-        setName(name);
-        setGender(gender);
-        setUserProfile(userProfile);
+    public void update(String name, String gender, UserProfile userProfile) {
+        this.name = name;
+        this.gender = gender;
+        this.userProfile = userProfile;
     }
 
     public String getId() {
         return id;
     }
 
-    protected void setId(String id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
     }
 
-    protected void setName(String name) {
-        this.name = name;
-    }
 
     public List<Card> getCards() {
         return cards;
+    }
+
+    private void setCards(List<Card> cards) {
+        cards.forEach(card -> card.setUserId(getId()));
+        this.cards = cards;
     }
 
     public UserProfile getUserProfile() {
         return userProfile;
     }
 
-    protected void setUserProfile(UserProfile userProfile) {
-        this.userProfile = userProfile;
-    }
-
     public String getGender() {
         return gender;
     }
 
-    protected void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    protected void setCards(List<Card> cards) {
-        cards.forEach(card -> card.setUserId(getId()));
-        this.cards = cards;
-    }
-
     public Timestamp getVersion() {
-        return Version;
+        return version;
     }
 
-    private void setVersion(Timestamp version) {
-        Version = version;
-    }
+//    protected void setVersion(Timestamp version) {
+//        this.version = version;
+//    }
 }
 
