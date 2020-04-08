97116c90ec0eef912b705c9e63aced87ec10475a
Ivan Zheng <yvonne_7_7@hotmail.com>
2020-04-08 17:59:24 +0800

    clean up code

--- src/SpringBootDemo.Portal/src/main/java/com/demo/portal/controllers/HomeController.java
+++ src/SpringBootDemo.Portal/src/main/java/com/demo/portal/controllers/HomeController.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 //import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,9 +27,10 @@ public class HomeController {
 //        String logoutUri = String.format("http://zero.teamcore.cn/ids/connect/endsession?post_logout_redirect_uri=%s&id_token_hint=%s",
 //               URLEncoder.encode("http://localhost:8082/home", StandardCharsets.UTF_8.toString()), tokenValue);
         OidcIdToken token = ((DefaultOidcUser)((OAuth2AuthenticationToken) principal).getPrincipal()).getIdToken();
-        model.addAttribute("username", principal == null ? "" : ((DefaultOidcUser) ((OAuth2AuthenticationToken) principal).getPrincipal()).getUserInfo().getFullName());
+        OidcUserInfo userInfo = ((DefaultOidcUser) ((OAuth2AuthenticationToken) principal).getPrincipal()).getUserInfo();
+        model.addAttribute("username", principal == null ? "" : (userInfo.getFullName()));
         //model.addAttribute("logoutUri", logoutUri);
-
+        model.addAttribute("userInfo", new ObjectMapper().writeValueAsString(userInfo.getClaims()));
         model.addAttribute("tokenValue", new ObjectMapper().writeValueAsString(token.getTokenValue()));
         model.addAttribute("claims", new ObjectMapper().writeValueAsString(token.getClaims()));
         return "home/index";
