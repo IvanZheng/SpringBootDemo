8cb06d470ffcbc3dd1d0c3246226cfd3e690c5da
Ivan Zheng <yvonne_7_7@hotmail.com>
2020-04-08 13:57:52 +0800

    clean up code

--- src/SpringBootDemo.Portal/src/main/java/com/demo/portal/controllers/HomeController.java
+++ src/SpringBootDemo.Portal/src/main/java/com/demo/portal/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.demo.portal.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Controller;
@@ -18,12 +21,14 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class HomeController {
 
     @RequestMapping(value = "", method = GET)
-    public String index(Principal principal, Model model) throws UnsupportedEncodingException {
-        String tokenValue = ((OAuth2AuthenticationDetails)((OAuth2Authentication) principal).getDetails()).getTokenValue();
+    public String index(Principal principal, Model model) throws UnsupportedEncodingException, JsonProcessingException {
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)((OAuth2Authentication) principal).getDetails();
+        String tokenValue = details.getTokenValue();
         String logoutUri = String.format("http://zero.teamcore.cn/ids/connect/endsession?post_logout_redirect_uri=%s&id_token_hint=%s",
                 URLEncoder.encode("http://localhost:8082/home", StandardCharsets.UTF_8.toString()), tokenValue);
         model.addAttribute("username", principal == null ? "" : principal.getName());
         model.addAttribute("logoutUri", logoutUri);
+        model.addAttribute("details", new ObjectMapper().writeValueAsString(((OAuth2Authentication) principal).getUserAuthentication().getDetails()));
         return "index";
     }
 }
